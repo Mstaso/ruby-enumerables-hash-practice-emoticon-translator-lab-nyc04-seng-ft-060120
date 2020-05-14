fe5be1ad331813b361a7e229d12a0f9df22c0e99
malcolm staso <mstaso1@gmail.com>
2020-05-14 18:21:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,8 +9,8 @@ def load_library(path)
  emoticon_hash["get_meaning"] = Hash.new
  
  emoticons.each do |english_word, emoticon_set|
-   :English = emoticon_hash[0]
-   :Japanese = emoticon_hash[1]
+   :English => emoticon_hash[0]
+   :Japanese => emoticon_hash[1]
    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
   end
