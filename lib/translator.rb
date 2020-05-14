979b0bf66da440c5026a482a727a05ebfc8527cf
malcolm staso <mstaso1@gmail.com>
2020-05-14 19:04:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'pry'
 
 def load_library(path)
- emoticons = YAML.load_file(path)
+ emoticons = YAML.load_file('./lib/emoticons.yml')
  emoticon_lib = {'get_meaning' => {}, 'get_emoticon' => {}}
  
 emoticons.each do |meaning, value|
