8d18a44392489cb9a9c948644929e155bf1d3835
malcolm staso <mstaso1@gmail.com>
2020-05-14 19:06:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,4 +16,9 @@ emoticons.each do |meaning, value|
 end
 
 def get_japanese_emoticon(emoticon_file = './lib/emoticons.yml', emoticon)
+   emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
+end
+
 
